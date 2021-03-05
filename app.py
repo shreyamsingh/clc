d272455fa82e74200f10c6c1c649c5c214e42bcb
shreyamsingh <shreyamsingh34@gmail.com>
2021-03-04 21:47:17 -0500

    Add comments

--- app.py
+++ app.py
@@ -3,15 +3,20 @@ import requests
 
 app = Flask(__name__)
 
+# default homepage
 @app.route('/')
 def homepage():
    return render_template('index.html')
 
+# dog page
 @app.route('/dog')
 def getDog():
-   return render_template('dog.html', url=requests.get('https://dog.ceo/api/breeds/image/random').json()['message'])
+   response = requests.get('https://dog.ceo/api/breeds/image/random')
+   imgURL = response.json()['message']
+   return render_template('dog.html', url=imgURL)
 
+# homepage if there's something after the / besides 'dog'
 @app.route('/<name>')
 def customHomepage(name):
-   return render_template('index.html', name=name)
+   return render_template('index.html', name=name.title())
    
